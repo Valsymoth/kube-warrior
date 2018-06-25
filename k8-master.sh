37bc13663cdc27ff1a1e3c8d1cc82ed821d50ea7
Tomislav Santic <35932169+Valsymoth@users.noreply.github.com>
2018-06-25 16:11:34 +0200

    Create k8-master.sh

---
+++ k8-master.sh
@@ -0,0 +1,39 @@
+setenforce 0 
+sed -i 's/SELINUX=.*/SELINUX=permissive/' /etc/selinux/config 
+yum install docker -y 
+systemctl enable docker
+systemctl start docker
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+yum install kubeadm kubectl kubelet -y 
+systemctl enable kubelet  
+systemctl start kubelet  
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system 
+kubeadm init --pod-network-cidr=10.244.0.0/16 
+# Master stuff
+chmod 640 /etc/sudoers 
+echo "kubewarrior ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+sed -i 's/%wheel.*/%wheel        ALL=(ALL)       NOPASSWD: ALL/' /etc/sudoers 
+chmod 440 /etc/sudoers 
+useradd -G wheel kubewarrior 
+echo kubewarrior:kubewarrior | chpasswd
+user=kubewarrior 
+mkdir -p /home/$user/.kube 
+cp -i /etc/kubernetes/admin.conf /home/$user/.kube/config
+chown $user:$user /home/$user/.kube/config
+sudo -u $user kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml 
+sudo -u $user kubectl get nodes 

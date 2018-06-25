76277d437d886577bbf480101e4b2f9cf45236c0
Tomislav Santic <35932169+Valsymoth@users.noreply.github.com>
2018-06-25 23:49:52 +0200

    Update k8-master.sh

--- k8-master.sh
+++ k8-master.sh
@@ -37,3 +37,8 @@ cp -i /etc/kubernetes/admin.conf /home/$user/.kube/config
 chown $user:$user /home/$user/.kube/config
 sudo -u $user kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml 
 sudo -u $user kubectl get nodes 
+kubeadm token create --print-join-command >> k8-worker.sh
+# hardcored, don't like this part much.
+scp ./k8-worker.sh root@207.154.243.210:~
+ssh root@46.101.198.252 "~/k8-worker.sh"
+ssh root@46.101.198.252 ". /root/k8-worker.sh"
